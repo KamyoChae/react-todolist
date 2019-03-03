aef3bcb46b911d5eb8636366efc6b3741817cfd2
kamyochae <1465678596@qq.com>
2019-03-03 21:39:41 +0800

    push build

---
+++ build/precache-manifest.2a6564bee9286202f1c955b7757a0e03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "54c460ca077dc81bda02",
+    "url": "/static/js/main.54c460ca.chunk.js"
+  },
+  {
+    "revision": "5fea0a9e6b8b3a0fccd4",
+    "url": "/static/js/2.5fea0a9e.chunk.js"
+  },
+  {
+    "revision": "54c460ca077dc81bda02",
+    "url": "/static/css/main.ec04df0b.chunk.css"
+  },
+  {
+    "revision": "9b44aa9373521c758cdaa581d1fff0cb",
+    "url": "/index.html"
+  }
+];
